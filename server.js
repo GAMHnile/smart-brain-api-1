3d893f4bb5c5e38496238e44adab237950da93bc
GAMHnile <georgeamhuweh@gmail.com>
2020-09-24 14:22:38 +0100

    added credentials as environmental variables, audited packages

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+require('dotenv').config();
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -13,11 +14,12 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
+    user : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PASS,
     database : 'smart-brain'
   }
 });
+
 
 const app = express();
 
@@ -31,6 +33,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(3500, ()=> {
+  console.log('app is running on port 3500');
 })
